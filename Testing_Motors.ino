268a622ca553baa166b54666b44168d9cbc6a599
AsadR110 <62395847+AsadR110@users.noreply.github.com>
2021-01-02 20:23:54 -0500

    Add files via upload

--- Testing_Motors.ino
+++ Testing_Motors.ino
@@ -1,68 +1,71 @@
 
 #include <EEPROM.h>
 #include <AFMotor.h>
-AF_DCMotor motor1(1);
-AF_DCMotor motor2(2);
-AF_DCMotor motor3(3);
-AF_DCMotor motor4(4);
+AF_DCMotor motor1(1); //TOP RIGHT
+AF_DCMotor motor2(2); //TOP LEFT
+AF_DCMotor motor3(3); //BOTTOM LEFT
+AF_DCMotor motor4(4); //BOTTOM RIGHT
 char Incoming_value = 0; // initialize the incoming the signal from the bluetooth as zero
 
 
 void setup() {
   // put your setup code here, to run once:
-
-Serial.begin(9600);
-}
-
-void forward() {
   motor1.setSpeed(250);
   motor2.setSpeed(250);
   motor3.setSpeed(250);
   motor4.setSpeed(250);
+  Serial.begin(9600);
+}
+
+void forward() {
+//  motor1.setSpeed(250);
+//  motor2.setSpeed(250);
+//  motor3.setSpeed(250);
+//  motor4.setSpeed(250);
 
   motor1.run(FORWARD);
   motor2.run(FORWARD);
-  motor3.run(FORWARD); // 
+  motor3.run(FORWARD);
   motor4.run(FORWARD);
   
 }
 
 void left() {
-
-  motor1.setSpeed(100);
-  motor2.setSpeed(100);
-  motor3.setSpeed(250);
-  motor4.setSpeed(250);
+// Right motors (1&4) go forward and left motors (2&3) go backwards
+//  motor1.setSpeed(250);
+//  motor2.setSpeed(250);
+//  motor3.setSpeed(250);
+//  motor4.setSpeed(250);
   
   motor1.run(FORWARD);
   motor2.run(FORWARD);
-  motor3.run(FORWARD); // 
+  motor3.run(FORWARD);
   motor4.run(FORWARD);
   
 }
 
 void right() {
-
-  motor1.setSpeed(250);
-  motor2.setSpeed(250);
-  motor3.setSpeed(100);
-  motor4.setSpeed(100);
+// Left motors (2&3) go forward and right motors (1&4) go backwards
+//  motor1.setSpeed(250);
+//  motor2.setSpeed(250);
+//  motor3.setSpeed(250);
+//  motor4.setSpeed(250);
   
   motor1.run(FORWARD);
-  motor2.run(FORWARD);
-  motor3.run(FORWARD); // 
+  motor2.run(BACKWARD);
+  motor3.run(BACKWARD); 
   motor4.run(FORWARD);
   
 }
 void backward() {
-  motor1.setSpeed(250);
-  motor2.setSpeed(250);
-  motor3.setSpeed(250);
-  motor4.setSpeed(250);
+//  motor1.setSpeed(250);
+//  motor2.setSpeed(250);
+//  motor3.setSpeed(250);
+//  motor4.setSpeed(250);
   
   motor1.run(BACKWARD);
   motor2.run(BACKWARD);
-  motor3.run(BACKWARD); // 
+  motor3.run(BACKWARD); 
   motor4.run(BACKWARD);
   
 }
@@ -73,24 +76,22 @@ void done() {
   motor4.run(RELEASE);
 }
 void loop() {
-  forward();
-//    if(Serial.available() > 0)
-//  {
-//    Incoming_value = Serial.read();
-//    Serial.print(Incoming_value);
-//    Serial.print('\n');
-//    if(Incoming_value == '1' )
-//      forward();
-//    else if(Incoming_value == '0')
-//      backward();
-//    else if(Incoming_value == '2')
-//      left(); 
-//    else if(Incoming_value == '3')
-//      right();  
-//     else if(Incoming_value == '4')
-//      done();  
-//  }
-  //digitalWrite(buttonPin, HIGH);
-  // put your main code here, to run repeatedly:
+    if(Serial.available() > 0)
+  {
+    Incoming_value = Serial.read();
+    Serial.print(Incoming_value);
+    Serial.print('\n');
+    if(Incoming_value == '1' )
+      forward();
+    else if(Incoming_value == '0')
+      backward();
+    else if(Incoming_value == '2')
+      left(); 
+    else if(Incoming_value == '3')
+      right();  
+     else if(Incoming_value == '4')
+      done();  
+  }
+
 
 }
